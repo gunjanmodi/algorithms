bdef488261e60803a6fa733e5029e38d3e61dd77
Gunjan Modi <er.gunjanmodi@gmail.com>
2021-11-17 12:40:41 +0530

    is isomorphic program improvement

--- programming/strings/is_isomorphic.py
+++ programming/strings/is_isomorphic.py
@@ -1,22 +1,15 @@
-def is_isomorphic(string1, string2):
-    # if len(string1) != len(string2):
-    #     return False
-    reference = {}
-    visited = {}
-    i = 0
-    while i < len(string1):
-        reference[i] = [string1[i], string2[i]]
-        if string1[i] in visited:
-            other = visited[string1[i]]
-            if reference[i] != reference[other]:
-                return False
-        if string2[i] in visited:
-            other = visited[string2[i]]
-            if reference[i] != reference[other]:
-                return False
-        visited[string1[i]] = i
-        visited[string2[i]] = i
-        i += 1
+from collections import defaultdict
+
+def is_isomorphic(s, t):
+    if len(s) != len(t):
+        return False
+    n = len(s)
+    m1, m2 = defaultdict(str), defaultdict(str)
+    for i in range(n):
+        if m1[s[i]] != m2[t[i]]:
+            return False
+        m1[s[i]] = i + 1
+        m2[t[i]] = i + 1
     return True
 
 
